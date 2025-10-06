25b70026844ae328f941e6412951d0a0f67b750e
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2025-10-06 15:17:35 +0700

    first commit

---
+++ prak1.py
@@ -0,0 +1,12 @@
+percentage = float(input("Masukkan persentase nilai siswa : "))
+
+if percentage >= 90 :
+    print ("Excelent performance")
+elif percentage >= 80 :
+    print ("Very Good Performance")
+elif percentage >= 70 :
+    print ("Good performance")
+elif percentage >= 60 :
+    print ("Average performance")
+else :
+    print ("Needs immprovement")
